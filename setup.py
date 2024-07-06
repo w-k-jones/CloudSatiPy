7d1525716dbc2394b66fd324c57c7c7385386e27
William Jones <william.jones@physics.ox.ac.uk>
2024-07-06 12:24:04 +0100

    Fix typo in setup file

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='cloudsatip',
+    name='cloudsatipy',
     version='0.0.1',
     url='https://github.com/w-k-jones/CloudSatiPy.git',
     author='William Jones',

b5e8f830ffd55e52465223685f222b051853ca65
William Jones <william.jones@physics.ox.ac.uk>
2024-07-06 17:50:21 +0100

    Update char variables to be read as strings

--- cloudsatipy/_CONSTANTS.py
+++ cloudsatipy/_CONSTANTS.py
@@ -7,15 +7,15 @@ from pyhdf.SD import SDC
 
 # Map VData types to numpy dtypes
 HC_TYPES = {
-    HC.CHAR: np.byte,
-    HC.CHAR8: np.byte,
+    HC.CHAR: str,
+    HC.CHAR8: str,
     HC.FLOAT32: np.float32,
     HC.FLOAT64: np.float64,
     HC.INT16: np.int16,
     HC.INT32: np.int32,
     HC.INT8: np.int8,
-    HC.UCHAR: np.byte,
-    HC.UCHAR8: np.byte,
+    HC.UCHAR: str,
+    HC.UCHAR8: str,
     HC.UINT16: np.uint16,
     HC.UINT32: np.uint32,
     HC.UINT8: np.uint8,
@@ -23,15 +23,15 @@ HC_TYPES = {
 
 # Map Scientific data types to numpy dtypes
 SDC_TYPES = {
-    SDC.CHAR: np.byte,
-    SDC.CHAR8: np.byte,
+    SDC.CHAR: str,
+    SDC.CHAR8: str,
     SDC.FLOAT32: np.float32,
     SDC.FLOAT64: np.float64,
     SDC.INT16: np.int16,
     SDC.INT32: np.int32,
     SDC.INT8: np.int8,
-    SDC.UCHAR: np.byte,
-    SDC.UCHAR8: np.byte,
+    SDC.UCHAR: str,
+    SDC.UCHAR8: str,
     SDC.UINT16: np.uint16,
     SDC.UINT32: np.uint32,
     SDC.UINT8: np.uint8,

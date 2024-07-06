a3f6bdec6b4812592a76f510b9eebe783ccd607b
William Jones <william.jones@physics.ox.ac.uk>
2024-07-06 12:15:23 +0100

    Add setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cloudsatip',
+    version='0.0.1',
+    url='https://github.com/w-k-jones/CloudSatiPy.git',
+    author='William Jones',
+    author_email='william.jones@physics.ox.ac.uk',
+    description='Read CloudSat HDF files into xarray datasets',
+    packages=find_packages(),    
+    install_requires=[],
+)

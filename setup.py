c0a166cc2f883b93afc00750bb18bb862b48ace5
William Jones <william.jones@physics.ox.ac.uk>
2024-07-08 13:05:47 +0100

    Bump version for initial release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cloudsatipy",
-    version="0.0.1",
+    version="0.1.0",
     url="https://github.com/w-k-jones/CloudSatiPy.git",
     author="William Jones",
     author_email="william.jones@physics.ox.ac.uk",
